a63d5625b111763f74e1a2783e72417c09b54ef6
Jacob <jacobcrawford1@hotmail.com>
2018-09-02 12:43:58 +0200

    Fixed

--- chapter4-gpios/sensor_reader_api.js
+++ chapter4-gpios/sensor_reader_api.js
@@ -1,27 +1,35 @@
-var sensorLib = require('node-dht-sensor');
-var onoff = require('onoff');
+var sensorLib = require("node-dht-sensor");
+var onoff = require("onoff");
 
 // The LED
 var Gpio = onoff.Gpio,
-  led = new Gpio(4, 'out') //#B
+  led = new Gpio(4, "out"); //#B
 
 // The humidity and temperature sensor
 sensorLib.initialize(22, 12);
 
-
 // Read the humidity and temperature from the sensor.
 function read() {
   var readout = sensorLib.read(); //#C
-  console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + //#D
-    'humidity: ' + readout.humidity.toFixed(2) + '%');
-  return {hum: readout.humidity.toFixed(2), temp: readout.temperature.toFixed(2)}
-};
+  console.log(
+    "Temperature: " +
+    readout.temperature.toFixed(2) +
+    "C, " + //#D
+      "humidity: " +
+      readout.humidity.toFixed(2) +
+      "%"
+  );
+  return {
+    hum: readout.humidity.toFixed(2),
+    temp: readout.temperature.toFixed(2)
+  };
+}
 
-process.on('SIGINT', function () {
+process.on("SIGINT", function() {
   clearInterval(interval);
   led.writeSync(0); //#G
   led.unexport();
-  console.log('Bye, bye!');
+  console.log("Bye, bye!");
   process.exit();
 });
 
@@ -30,27 +38,29 @@ process.on('SIGINT', function () {
 //#C read the sensor values
 //#D readout contains two values: temperature and humidity
 
-
 // Start the server to handle the requests
 
 var http = require("http");
 http
   .createServer(function(req, res) {
-	switch(req.method) {
-	case 'GET':
-    		res.writeHeader(200, { "Content-Type": "application/json" });
-    		data = read();
-		res.end(JSON.stringify({
-			"led":  led.readSync(),
-			"hum":  data.hum,
-			"temp": data.temp
-		});
-		break;
-  	case 'POST':
-		res.writeHeader(400, {"Content-Type": "plain/text"});
-		res.end("Unsupported");
-  		break;
-	}
-  }).listen(8585);
+    switch (req.method) {
+      case "GET":
+        res.writeHeader(200, { "Content-Type": "application/json" });
+        data = read();
+        res.end(
+          JSON.stringify({
+            led: led.readSync(),
+            hum: data.hum,
+            temp: data.temp
+          })
+        );
+        break;
+      case "POST":
+        res.writeHeader(400, { "Content-Type": "plain/text" });
+        res.end("Unsupported");
+        break;
+    }
+  })
+  .listen(8585);
 
 console.log("Server started");
